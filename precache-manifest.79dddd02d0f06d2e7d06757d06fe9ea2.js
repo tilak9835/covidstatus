2fc519d8fb8126e366572a7bd2fe4488378f4617
tilak9835 <tilak.echarge@gmail.com>
2020-08-05 00:46:23 +0530

    Updates

---
+++ precache-manifest.79dddd02d0f06d2e7d06757d06fe9ea2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f691e10bea5d1d32d421470ba205bbca",
+    "url": "/covidstatus/index.html"
+  },
+  {
+    "revision": "65fa13d09527bc5f8d4b",
+    "url": "/covidstatus/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "223bc1d72546664800ff",
+    "url": "/covidstatus/static/css/main.2229a4df.chunk.css"
+  },
+  {
+    "revision": "65fa13d09527bc5f8d4b",
+    "url": "/covidstatus/static/js/2.4dd8868e.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/covidstatus/static/js/2.4dd8868e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223bc1d72546664800ff",
+    "url": "/covidstatus/static/js/main.f599ed29.chunk.js"
+  },
+  {
+    "revision": "086a45bc294f7619f5fa",
+    "url": "/covidstatus/static/js/runtime-main.b01ba3c9.js"
+  }
+]);
